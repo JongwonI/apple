4b5fea6005a159f686d0d9442f4f2c2ebc2c9c05
JongwonI <whddnjs942@gmail.com>
2024-03-25 16:25:16 +0900

    20240325

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient } = require('mongodb')
+const { MongoClient,ObjectId  } = require('mongodb')
 
 let db
 const url = 'mongodb+srv://whddnjs942:sa963852!!@imjw.aaaroyx.mongodb.net/?retryWrites=true&w=majority&appName=Imjw'
@@ -16,9 +16,11 @@ new MongoClient(url).connect().then((client)=>{
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) 
 
 app.get('/', (요청, 응답) => {
-  응답.sendFile(__dirname + '/test.html')
+  응답.render('main.ejs')
 }) 
 
 app.get('/intro', (요청, 응답) => {
@@ -32,9 +34,59 @@ app.get('/shop', (요청, 응답) => {
 
 app.get('/list', async (요청, 응답) => {
     let result = await db.collection('user').find().toArray()
-    응답.render('list.ejs',{posts : result})
+    응답.render('list.ejs',{result : result})
 }) 
+app.get('/edit/:id', async (요청, 응답) => {
+    let result = await db.collection('user').findOne({_id: new ObjectId(요청.params.id)})
+    console.log(result)
+    응답.render('edit.ejs',{result : result})
+}) 
+app.post('/contentedit',async (요청,응답)=>{
+    console.log(요청.body)
+    try{
+        db.collection('user').updateOne(
+            {_id: new ObjectId(요청.body._id)},
+            {$set : {title : 요청.body.title,content : 요청.body.content}}
+            )
+        응답.redirect('/list')
+    }catch(e){
+        응답.status(400).send('뭔가 잘못됨')
+    }
+})
+
+app.get('/write', (요청, 응답) => {
+    응답.render('write.ejs')
+}) 
+app.post('/add', async (요청,응답)=>{
+    console.log(요청.body)
+    
+    try{
+        if(요청.body.title == ''){
+            응답.send('제목다시쓰세용')
+        }else{
+            await db.collection('user').insertOne({title:요청.body.title , content:요청.body.content})
+            응답.redirect('/list')
+        }
+    }catch(e){
+        console.log(e)
+        응답.status(500).redirect('/write')
+    }
+})
+
 app.get('/time', async (요청, 응답) => {
     let time = new Date()
     응답.render('time.ejs',{time : time})
+}) 
+app.get('/detail/:id', async(요청, 응답) => {
+    try{
+        let result = await db.collection('user').findOne({_id : new ObjectId(요청.params.id)}) 
+        if(result == null){
+            응답.status(400).send('url이 이상해요')
+        }
+        응답.render('detail.ejs',{result : result})
+    }
+    catch(e){
+        console.log(e)
+        응답.status(400).send('url이 이상해요')
+    }
 }) 
